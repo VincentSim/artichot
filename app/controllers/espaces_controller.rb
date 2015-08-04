27329e86baa0df675ade24b6981aa22887d31448
Vincent Simon <vincent.simon@gadz.org>
2015-08-04 09:06:10 +0200

    devise css

--- app/controllers/espaces_controller.rb
+++ app/controllers/espaces_controller.rb
@@ -1,6 +1,6 @@
 class EspacesController < ApplicationController
   before_action :set_espace, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, ,:create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   respond_to :html
 
