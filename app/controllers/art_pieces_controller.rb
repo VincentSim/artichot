b584f1e4b0bef9a29719d3a9ac08eb9cef56de4f
Vincent Simon <vincent.simon@gadz.org>
2015-08-13 13:07:09 +0200

    ajouter supprimer une oeuvre c'est okay

--- app/controllers/art_pieces_controller.rb
+++ app/controllers/art_pieces_controller.rb
@@ -1,5 +1,5 @@
 class ArtPiecesController < ApplicationController
-  before_action :set_espace, only: [:destroy]
+  before_action :set_art_piece, only: [:destroy]
   before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
@@ -18,9 +18,18 @@ class ArtPiecesController < ApplicationController
     end
   end
 
+  def destroy
+    @espace = @art_piece.espace
+    @art_piece.destroy
+      respond_to do |format|
+        format.html { redirect_to espace_path(@espace), notice: 'You removed your art_piece' }
+        format.js
+    end
+  end
+
   private
     def set_art_piece
-      @art_piece = art_piece.find(params[:id])
+      @art_piece = ArtPiece.find(params[:id])
     end
 
     def art_piece_params
