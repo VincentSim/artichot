f4d8fa0470156b61c40e6758b194781ae5abd989
Vincent Simon <vincent.simon@gadz.org>
2015-08-24 15:14:25 +0200

    remodelage des photos

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   validates :encrypted_password, presence: true
 
   has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" }
+    styles: { medium: "300x300#", thumb: "100x100#" }
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
