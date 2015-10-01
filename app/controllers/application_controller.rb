9861072fda926e6da60405bd42b42dc4ff6cd8a4
Vincent Simon <vincent.simon@gadz.org>
2015-10-01 15:10:24 +0200

    changements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,9 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    if extract_locale_from_tld
+    if extract_locale_from_tld && params[:locale] == :en
+      I18n.locale = :en
+    elsif extract_locale_from_tld
       I18n.locale = extract_locale_from_tld
     elsif I18n.locale == :fr && params[:locale] == nil
       I18n.locale = :fr
